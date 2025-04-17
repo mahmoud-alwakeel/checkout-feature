da8f323dc8a8f53b95f8fe8c7792511c2ee70b29
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-04-18 01:29:28 +0200

    not going to credit card screen

--- lib/features/checkout/presentation/screens/my_cart_screen.dart
+++ lib/features/checkout/presentation/screens/my_cart_screen.dart
@@ -1,7 +1,7 @@
 import 'package:checkout_feature/common/build_app_bar.dart';
 import 'package:checkout_feature/common/custom_button.dart';
-import 'package:checkout_feature/features/checkout/presentation/screens/payment_details_screen.dart';
 import 'package:checkout_feature/features/checkout/presentation/widgets/order_info_item.dart';
+import 'package:checkout_feature/features/checkout/presentation/widgets/payment_methods_list_view.dart';
 import 'package:checkout_feature/features/checkout/presentation/widgets/total_price.dart';
 import 'package:flutter/material.dart';
 
@@ -54,14 +54,17 @@ class MyCartScreen extends StatelessWidget {
             CustomButton(
               title: 'Complete Payment',
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const PaymentDetailsScreen();
-                    },
-                  ),
-                );
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) {
+                //       return const PaymentDetailsScreen();
+                //     },
+                //   ),
+                // );
+                showModalBottomSheet(context: context, builder: (context) {
+                  return PaymentmethodsBottomSheet();
+                });
               },
             ),
             const SizedBox(
@@ -69,6 +72,25 @@ class MyCartScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PaymentmethodsBottomSheet extends StatelessWidget {
+  const PaymentmethodsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          PaymentMethodsListView(),
+          SizedBox(height: 32,),
+          CustomButton(onTap: (){}, title: 'continue')
+        ],
       ),
     );
   }
